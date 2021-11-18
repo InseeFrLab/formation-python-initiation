64582dd45412e0cf2005120b0fc73bc0cd4a3e64
avouacr <avouacr@pm.me>
2021-11-18 10:38:13 +0000

    handle possible requirements.txt in init script

--- init-onyxia.sh
+++ init-onyxia.sh
@@ -16,6 +16,11 @@ cp ${COURSE_DIR}/${CHAPTER}/${IPYNB_NAME}.ipynb ${FORMATION_DIR}/
 cp ${COURSE_DIR}/${CHAPTER}/solutions/${IPYNB_NAME}.py ${WORK_DIR}/solutions.py
 # Give appropriate permissions to allow editing notebook
 chown -R jovyan:users $FORMATION_DIR
+
+# Install additional packages if needed
+REQUIREMENTS_FILE=${COURSE_DIR}/${CHAPTER}/requirements/${IPYNB_NAME}.txt
+[ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
+
 # Remove clone repository
 rm -r $CLONE_DIR
 
