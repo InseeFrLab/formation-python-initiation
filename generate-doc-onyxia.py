25727a0b47439c27cda7da07d2da15de60a6e66f
avouacr <avouacr@pm.me>
2021-11-19 16:59:16 +0000

    improve doc generation, but wip

--- generate-doc-onyxia.py
+++ generate-doc-onyxia.py
@@ -4,7 +4,7 @@ import os
 import json 
 
 def generate_block(name, abstract, authors, contributors, types, tags, category,
-                   image_url, article_url=None, deployment_url=None):
+                   img_url, article_url=None, deployment_url=None):
     """Generate json documentation for a training program block.
     
     If both `article_url` and `deployment_url` are None, the block is assumed
@@ -20,7 +20,7 @@ def generate_block(name, abstract, authors, contributors, types, tags, category,
         "types": types,
         "tags": tags,
         "category": category,
-        "imageUrl": image_url
+        "img_url": img_url
     }
 
     if article_url is None and deployment_url is None:
@@ -33,46 +33,38 @@ def generate_block(name, abstract, authors, contributors, types, tags, category,
     return block
 
 
-def get_metadata_ipynb(ipynb_path):
-    """Extract title and abstract from the metadata of a Jupyter Notebook.
-    
-    An error is raised if these metadata are not present.
-    """
-    with open(ipynb_path, "r") as file:
-        ipynb_json = json.load(file)
-    ipynb_metadata = ipynb_json["metadata"]
-
-    if "title" not in ipynb_metadata.keys() or "abstract" not in ipynb_metadata.keys():
-        raise KeyError("'title' and 'abstract' fields must be set in each notebook's metadata.")
-
-    return ipynb_metadata["title"], ipynb_metadata["abstract"]
-
-
-
-
-
-
-CWD = os.getcwd()
-COURSE_DIR = os.path.join(CWD, "course")
-chapters = [ch for ch in COURSE_DIR if os.path.isdir(os.path.join(COURSE_DIR, ch))]
-for chapter in chapters:
-    dir_chapter = os.path.join(COURSE_DIR, chapter)
-    ipynb_paths = [f for f in os.listdir(dir_chapter) if f.endswith(".ipynb")]
-    for ipynb in ipynb_paths:
-        ipynb_name = ipynb.split(".")[0]
-        solution_files = os.listdir(os.path.join(dir_chapter, "solutions"))
-        solution_py = ipynb_name + ".py"
-        if solution_py not in solution_files:
-            path_missing = os.path.join(dir_chapter, "solutions", solution_py)
-            raise FileNotFoundError(f"{path_missing} not found.")
-
-
 if __name__ == "__main__":
-    FORMATION = "Initiation à Python"
-    ABSTRACT = "Cours introductif à Python : fondamentaux du langage et premières manipulations de données"
-    AUTHORS = ["inseefrlab"]
-    CONTRIBUTORS = ["Romain Avouac, Julie Djiriguian, Yves-Laurent Bénichou, Lino Galiana"]
-    TYPES = ["Site documentaire"]
-    TAGS = ["discover", "learn"]
-    CATEGORY = "datascience with R and Python"
-    IMG_URL = "https://raw.githubusercontent.com/InseeFrLab/www.sspcloud.fr/main/src/app/assets/img/python.jpg"
+
+    PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    # Build documentation's top block
+    with open(os.path.join(PROJECT_DIR, "METADATA.json"), "r") as file:
+        md = json.load(file)
+    doc_json = generate_block(name=md["name"], 
+                              abstract=md["abstract"], 
+                              authors=md["authors"], 
+                              contributors=md["contributors"], 
+                              types=md["types"], 
+                              tags=md["tags"], 
+                              category=md["category"], 
+                              img_url=md["img_url"]
+                              )
+    for section in md["sections"].keys():
+        # Build section block
+        section_md = md["sections"][section]
+        section_doc = generate_block(name=section,
+                                     abstract=section_md["abstract"],
+                                     authors=md["authors"], 
+                                     contributors=md["contributors"],
+                                     types=md["types"], 
+                                     tags=md["tags"], 
+                                     category=md["category"],
+                                     img_url=md["img_url"]
+                                     )
+        
+        if "chapters" in section_md:
+            for chapter in section_md["chapters"].keys():
+                # Build chapter block
+                pass
+        doc_json["parts"].append(section_doc)
+                
