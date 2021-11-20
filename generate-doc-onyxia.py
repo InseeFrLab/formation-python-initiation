c8668793e0d14a594be67fa2742995a8f240e425
avouacr <avouacr@pm.me>
2021-11-20 14:38:00 +0000

    doc generation works, need to be tested

--- generate-doc-onyxia.py
+++ generate-doc-onyxia.py
@@ -1,7 +1,9 @@
 """Generate doc of a training program for https://www.sspcloud.fr."""
 
 import os
-import json 
+import json
+import urllib
+
 
 def generate_block(name, abstract, authors, contributors, types, tags, category,
                    img_url, article_url=None, deployment_url=None):
@@ -35,11 +37,21 @@ def generate_block(name, abstract, authors, contributors, types, tags, category,
 
 if __name__ == "__main__":
 
+    # Load course metadata
     PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-    # Build documentation's top block
     with open(os.path.join(PROJECT_DIR, "METADATA.json"), "r") as file:
         md = json.load(file)
+
+    # Main URLs
+    LAUNCHER_TMPLT = ("https://datalab.sspcloud.fr/launcher/inseefrlab-helm-charts-datascience/"
+                      "jupyter?init.personalInit=%C2%ABhttps%3A%2F%2Fraw.githubusercontent.com"
+                      "%2FInseeFrLab%2Fformation-python-initiation%2Fmain%2Finit-onyxia.sh%C2%BB"
+                      "&init.personalInitArgs=%C2%AB{init_args}%C2%BB&security.allowlist.enabled=false")
+    COURSE_NAME_ENCODED = urllib.parse.quote(md['name'])
+    DEFAULT_URL = f"https://www.sspcloud.fr/documentation?search=&path=%5B%22{COURSE_NAME_ENCODED}%22%5D"
+
+
+    # Build documentation's top block
     doc_json = generate_block(name=md["name"], 
                               abstract=md["abstract"], 
                               authors=md["authors"], 
@@ -52,19 +64,45 @@ if __name__ == "__main__":
     for section in md["sections"].keys():
         # Build section block
         section_md = md["sections"][section]
-        section_doc = generate_block(name=section,
-                                     abstract=section_md["abstract"],
-                                     authors=md["authors"], 
-                                     contributors=md["contributors"],
-                                     types=md["types"], 
-                                     tags=md["tags"], 
-                                     category=md["category"],
-                                     img_url=md["img_url"]
-                                     )
-        
         if "chapters" in section_md:
+            section_doc = generate_block(name=section_md["name"],
+                                         abstract=section_md["abstract"],
+                                         authors=md["authors"], 
+                                         contributors=md["contributors"],
+                                         types=md["types"], 
+                                         tags=md["tags"], 
+                                         category=md["category"],
+                                         img_url=md["img_url"]
+                                         )
             for chapter in section_md["chapters"].keys():
                 # Build chapter block
-                pass
+                chapter_md = section_md["chapters"][chapter]
+                launcher_url = LAUNCHER_TMPLT.format(init_args=f"{section} {chapter}")
+                chapter_doc = generate_block(name=chapter_md["name"],
+                                             abstract=chapter_md["abstract"],
+                                             authors=md["authors"],
+                                             contributors=md["contributors"],
+                                             types=md["types"],
+                                             tags=md["tags"],
+                                             category=md["category"],
+                                             img_url=md["img_url"],
+                                             deployment_url=launcher_url
+                                             )
+                section_doc["parts"].append(chapter_doc)
+        else:
+            launcher_url = LAUNCHER_TMPLT.format(init_args=f"{section}")
+            section_doc = generate_block(name=section_md["name"],
+                                         abstract=section_md["abstract"],
+                                         authors=md["authors"], 
+                                         contributors=md["contributors"],
+                                         types=md["types"], 
+                                         tags=md["tags"], 
+                                         category=md["category"],
+                                         img_url=md["img_url"],
+                                         deployment_url=launcher_url
+                                         )
         doc_json["parts"].append(section_doc)
-                
+
+    # Export doc
+    with open(os.path.join(PROJECT_DIR, "doc-onyxia.json"), "w") as file:
+        json.dump(doc_json, file, indent=4)
