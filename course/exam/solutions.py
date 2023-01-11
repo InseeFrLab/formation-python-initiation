2a3a018dee48ad914791959f0c655cf47c3a7522
avouacr <avouacr@pm.me>
2023-01-11 12:47:02 +0000

    review partie initialisation

---
+++ course/exam/solutions.py
@@ -0,0 +1,9 @@
+import sys
+
+def initialise_grille(n_lignes=6, n_colonnes=7):
+    grille = []
+
+    for ligne in range(n_lignes):
+        colonne_espace_vide = [' ' for espace in range(n_colonnes)]
+        grille.append(colonne_espace_vide)
+    return grille
