268c6eb774154aab76c001ad6a6306093975ac02
avouacr <avouacr@pm.me>
2023-10-16 13:39:20 +0000

    feat: transition to quarto

--- utils/init_onyxia.sh
+++ utils/init_onyxia.sh
@@ -6,23 +6,17 @@ CHAPTER=$2
 WORK_DIR=/home/onyxia/work
 CLONE_DIR=${WORK_DIR}/repo-git
 COURSE_DIR=${CLONE_DIR}/source
-FORMATION_DIR=${WORK_DIR}/formation
 
 # Clone course repository
 REPO_URL=https://github.com/InseeFrLab/formation-python-initiation.git
 git clone --depth 1 $REPO_URL $CLONE_DIR
 
-# Convert .md to .ipynb
-pip install python-frontmatter jupytext
-python $CLONE_DIR/utils/md_to_ipynb.py $COURSE_DIR/${SECTION}/${CHAPTER}/tutorial.md
-rm $COURSE_DIR/${SECTION}/${CHAPTER}/tutorial.md
-
 # Put chapter data in the training dir
-mkdir $FORMATION_DIR
-cp ${COURSE_DIR}/${SECTION}/${CHAPTER}/* ${FORMATION_DIR}/
+rm $COURSE_DIR/${SECTION}/${CHAPTER}/tutorial.qmd
+cp ${COURSE_DIR}/${SECTION}/${CHAPTER}/* ${WORK_DIR}/
 
 # Give write permissions
-chown -R onyxia:users $FORMATION_DIR/
+chown -R onyxia:users ${WORK_DIR}/
 
 # Install additional packages if needed
 REQUIREMENTS_FILE=${FORMATION_DIR}/requirements.txt
