8e6d3e9562d8a19e3a837131eeeaf56d923ee399
avouacr <avouacr@pm.me>
2024-07-31 10:20:12 +0000

    fix init

--- utils/init_onyxia.sh
+++ utils/init_onyxia.sh
@@ -11,6 +11,10 @@ CLONE_DIR=${WORK_DIR}/repo-git
 REPO_URL=https://github.com/InseeFrLab/formation-python-initiation.git
 git clone --depth 1 $REPO_URL $CLONE_DIR
 
+# Install additional packages if a requirements.txt file is present
+REQUIREMENTS_FILE=${CLONE_DIR}/requirements.txt
+[ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
+
 # Build notebook from sources
 SOURCE_FILE=${CLONE_DIR}/source/${SECTION}/${CHAPTER}/tutorial.qmd
 quarto render $SOURCE_FILE --profile ${LANGUAGE} --no-execute --to ipynb
@@ -18,11 +22,7 @@ quarto render $SOURCE_FILE --profile ${LANGUAGE} --no-execute --to ipynb
 # Put chapter files in the training dir
 cp -r ${CLONE_DIR}/_site/source/${SECTION}/${CHAPTER}/* ${WORK_DIR}/
 
-# Install additional packages if needed
-REQUIREMENTS_FILE=${CLONE_DIR}/requirements.txt
-[ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE && rm $REQUIREMENTS_FILE
-
-# Remove course Git repository
+# Remove Git repository
 rm -r $CLONE_DIR
 
 # Open the relevant notebook when starting Jupyter Lab
