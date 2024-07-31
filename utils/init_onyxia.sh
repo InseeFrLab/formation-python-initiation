ee117b833b60e6e3d132ecaea041d70ae0da1bbc
avouacr <avouacr@pm.me>
2024-07-31 19:20:24 +0000

    fix init

--- utils/init_onyxia.sh
+++ utils/init_onyxia.sh
@@ -6,6 +6,7 @@ CHAPTER=$3
 
 WORK_DIR=/home/onyxia/work
 CLONE_DIR=${WORK_DIR}/repo-git
+CHAPTER_DIR=${CLONE_DIR}/source/${SECTION}/${CHAPTER}
 
 # Clone course repository
 REPO_URL=https://github.com/InseeFrLab/formation-python-initiation.git
@@ -16,22 +17,13 @@ REQUIREMENTS_FILE=${CLONE_DIR}/requirements.txt
 [ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
 
 # Build notebook from sources
-SOURCE_FILE=${CLONE_DIR}/source/${SECTION}/${CHAPTER}/tutorial.qmd
-quarto render $SOURCE_FILE --profile ${LANGUAGE} --no-execute --to ipynb
+quarto render ${CHAPTER_DIR}/tutorial.qmd --profile ${LANGUAGE} --no-execute --to ipynb --output-dir .
 
 # Put chapter files in the training dir
-if [ "$LANGUAGE" = "fr" ]; then
-  DIR_CHAPTER_FILES="${CLONE_DIR}/_site/source/${SECTION}/${CHAPTER}"
-elif [ "$LANGUAGE" = "en" ]; then
-  DIR_CHAPTER_FILES="${CLONE_DIR}/_site/en/source/${SECTION}/${CHAPTER}"
-else
-  echo "Unsupported language: $LANGUAGE"
-  exit 1
-fi
-cp -r ${DIR_CHAPTER_FILES}/* ${WORK_DIR}/
+cp -r ${CHAPTER_DIR}/* ${WORK_DIR}/
 
-# Remove Git repository
-rm -r $CLONE_DIR
+# Clean
+rm -rf $CLONE_DIR ${WORK_DIR}/tutorial.qmd
 
 # Open the relevant notebook when starting Jupyter Lab
 echo "c.LabApp.default_url = '/lab/tree/tutorial.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
